3e192c5a34dc21c515f22acf8a188134588a39df
Pedro H S Teixeira <pedro.t@gmail.com>
2017-01-02 14:18:00 -0200

    v1.1.1

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def install_requires():
 
 setup(
     name="django-cubes",
-    version="1.1.0",
+    version="1.1.1",
     url='https://github.com/intelie/django-cubes',
     author='Vitor M. A. da Cruz',
     author_email='vitor.mazzi@intelie.com.br',
